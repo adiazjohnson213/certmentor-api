4de57452fb9ff91fd865a33b1bdd3d18fc95e0e1
adiazjohnson213 <adiazjohnson213@gmail.com>
2026-05-19 12:30:54 -0400

    feat(application): add StudyPlanResponse and StudyPlanItemResponse DTOs

---
+++ CertMentor.Application/DTOs/Exam/StudyPlanItemResponse.cs
@@ -0,0 +1,4 @@
+﻿namespace CertMentor.Application.DTOs.Exam
+{
+    public record StudyPlanItemResponse(string MicrosoftLearnUnit, int TimeInMinutes, bool IsCompleted);
+}
---
+++ CertMentor.Application/DTOs/Exam/StudyPlanResponse.cs
@@ -0,0 +1,4 @@
+﻿namespace CertMentor.Application.DTOs.Exam
+{
+    public record StudyPlanResponse(int Id, DateTime CreatedAt, IReadOnlyCollection<StudyPlanItemResponse> StudyPlanItems);
+}
