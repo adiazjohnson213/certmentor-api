dcaf5850b1fdd99a247d3b68d65d205f38fc520d
adiazjohnson213 <adiazjohnson213@gmail.com>
2026-05-19 12:31:16 -0400

    feat(application): add ICatalogService and IExamService interfaces

---
+++ CertMentor.Application/Interfaces/Catalog/ICatalogService.cs
@@ -0,0 +1,11 @@
+﻿using CertMentor.Application.DTOs.Catalog;
+
+namespace CertMentor.Application.Interfaces.Catalog
+{
+    public interface ICatalogService
+    {
+        Task<IReadOnlyCollection<CertificationResponse>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<CertificationResponse> GetByCodeAsync(string certificationCode, CancellationToken cancellationToken = default);
+        Task SyncAsync(string certificationCode, CancellationToken cancellationToken = default);
+    }
+}
---
+++ CertMentor.Application/Interfaces/Exam/IExamService.cs
@@ -0,0 +1,12 @@
+﻿using CertMentor.Application.DTOs.Exam;
+
+namespace CertMentor.Application.Interfaces.Exam
+{
+    public interface IExamService
+    {
+        Task<ExamSessionResponse> GenerateExamAsync(CreateExamSessionRequest createExamSessionRequest, CancellationToken cancellationToken = default);
+        Task<PerformanceRecordResponse> ValidateAnswersAsync(SubmitExamAnswersRequest submitExamAnswersRequest, CancellationToken cancellationToken = default);
+        Task<PerformanceRecordResponse> GeneratePerformanceReportAsync(int examSessionId, CancellationToken cancellationToken = default);
+        Task<StudyPlanResponse> GenerateStudyPlanAsync(int examSessionId, CancellationToken cancellationToken = default);
+    }
+}
