7a3b0184e5a58a93458c5d1a2377fa673341c18d
adiazjohnson213 <adiazjohnson213@gmail.com>
2026-05-20 09:04:18 -0400

    feat(application): add IExamAgentService

---
+++ CertMentor.Application/Interfaces/Exam/IExamAgentService.cs
@@ -0,0 +1,16 @@
+﻿using CertMentor.Application.DTOs.Exam;
+
+namespace CertMentor.Application.Interfaces.Exam
+{
+    public interface IExamAgentService
+    {
+        Task<IReadOnlyCollection<ExamQuestionResponse>> GenerateQuestionsAsync(
+            string context,
+            string certificationCode,
+            CancellationToken cancellationToken = default);
+
+        Task<StudyPlanResponse> GenerateStudyPlanAsync(
+            PerformanceRecordResponse performanceRecord,
+            CancellationToken cancellationToken = default);
+    }
+}
