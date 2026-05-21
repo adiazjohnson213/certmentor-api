f50fceb7e15704b30cad2d28b4bc702f78009f7d
adiazjohnson213 <adiazjohnson213@gmail.com>
2026-05-21 09:51:38 -0400

    feat(infrastructure): add ExamSessionRepository and PerformanceRecordRepository.

---
+++ CertMentor.Infrastructure/Repositories/Exam/ExamSessionRepository.cs
@@ -0,0 +1,24 @@
+﻿using CertMentor.Application.Interfaces.Exam;
+using CertMentor.Domain.Entities.Exam;
+using CertMentor.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace CertMentor.Infrastructure.Repositories.Exam
+{
+    public class ExamSessionRepository : BaseRepository<ExamSession>, IExamSessionRepository
+    {
+        public ExamSessionRepository(CertMentorDbContext certMentorDbContext) : base(certMentorDbContext)
+        {
+        }
+
+        public async Task<IReadOnlyCollection<ExamSession>> GetByCertificationIdAsync(int certificationId, CancellationToken cancellationToken = default)
+        {
+            return (await _certMentorDbContext.ExamSessions.Where(es => es.CertificationId == certificationId).AsNoTracking().ToListAsync(cancellationToken)).AsReadOnly();
+        }
+
+        public async Task<ExamSession?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            return await _certMentorDbContext.ExamSessions.FindAsync(new object[] { id }, cancellationToken);
+        }
+    }
+}
---
+++ CertMentor.Infrastructure/Repositories/Exam/PerformanceRecordRepository.cs
@@ -0,0 +1,24 @@
+﻿using CertMentor.Application.Interfaces.Exam;
+using CertMentor.Domain.Entities.Exam;
+using CertMentor.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace CertMentor.Infrastructure.Repositories.Exam
+{
+    public class PerformanceRecordRepository : BaseRepository<PerformanceRecord>, IPerformanceRecordRepository
+    {
+        public PerformanceRecordRepository(CertMentorDbContext certMentorDbContext) : base(certMentorDbContext)
+        {
+        }
+
+        public async Task<IReadOnlyCollection<PerformanceRecord>> GetAllAsync(CancellationToken cancellationToken = default)
+        {
+            return (await _certMentorDbContext.PerformanceRecords.AsNoTracking().ToListAsync(cancellationToken)).AsReadOnly();
+        }
+
+        public async Task<PerformanceRecord?> GetByExamSessionIdAsync(int examSessionId, CancellationToken cancellationToken = default)
+        {
+            return await _certMentorDbContext.PerformanceRecords.SingleOrDefaultAsync(pr => pr.ExamSessionId == examSessionId, cancellationToken);
+        }
+    }
+}
