50056422af3d0c55bfe9ab1a6c740cd452fe277a
adiazjohnson213 <adiazjohnson213@gmail.com>
2026-05-23 20:56:47 -0400

    feat(application): add IMicrosoftLearnClient

---
+++ CertMentor.Application/Interfaces/Catalog/IMicrosoftLearnClient.cs
@@ -0,0 +1,9 @@
+﻿using CertMentor.Domain.Entities.Catalog;
+
+namespace CertMentor.Application.Interfaces.Catalog
+{
+    public interface IMicrosoftLearnClient
+    {
+        Task<IReadOnlyCollection<Certification>> GetCertificationsAsync(CancellationToken cancellationToken = default);
+    }
+}
