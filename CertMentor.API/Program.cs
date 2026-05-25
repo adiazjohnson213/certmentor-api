38c4bed1c22b17e19346acdb56042146af2d35c1
adiazjohnson213 <adiazjohnson213@gmail.com>
2026-05-25 09:11:05 -0400

    feat(api): add services to the container

--- CertMentor.API/Program.cs
+++ CertMentor.API/Program.cs
@@ -1,6 +1,32 @@
+using CertMentor.Application.Interfaces.Catalog;
+using CertMentor.Application.Interfaces.Exam;
+using CertMentor.Infrastructure.ExternalServices.MicrosoftLearn.Clients;
+using CertMentor.Infrastructure.Persistence;
+using CertMentor.Infrastructure.Repositories.Catalog;
+using CertMentor.Infrastructure.Repositories.Exam;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<CertMentorDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddHttpClient<IMicrosoftLearnClient, MicrosoftLearnClient>(client =>
+{
+    client.BaseAddress = new Uri("https://learn.microsoft.com/api/catalog/");
+    client.DefaultRequestHeaders.Add("Accept", "application/json");
+});
+
+// TODO: Register services when implemented
+// builder.Services.AddScoped<ICatalogService, CatalogService>();
+// builder.Services.AddScoped<IExamService, ExamService>();
+
+builder.Services.AddScoped<ICertificationRepository, CertificationRepository>();
+builder.Services.AddScoped<ISkillAreaRepository, SkillAreaRepository>();
+builder.Services.AddScoped<ITopicRepository, TopicRepository>();
+builder.Services.AddScoped<IExamSessionRepository, ExamSessionRepository>();
+builder.Services.AddScoped<IPerformanceRecordRepository, PerformanceRecordRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
