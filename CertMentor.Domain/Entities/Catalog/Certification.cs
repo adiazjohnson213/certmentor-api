d19301de5b802ace4b03ace56b3fc91db733c176
adiazjohnson213 <adiazjohnson213@gmail.com>
2026-06-06 18:37:38 -0400

    feat(domain): add ExamDurationInMinutes and CalculateQuestionCount to Certification

--- CertMentor.Domain/Entities/Catalog/Certification.cs
+++ CertMentor.Domain/Entities/Catalog/Certification.cs
@@ -7,21 +7,24 @@
         public int Id { get; private set; }
         public string Code { get; private set; } = string.Empty;
         public string Name { get; private set; } = string.Empty;
+        public int ExamDurationInMinutes { get; private set; }
         public bool IsActive { get; private set; } = true;
 
         private Certification()
         {
         }
 
-        public static Certification Create(string code, string name)
+        public static Certification Create(string code, string name, int examDurationInMinutes)
         {
             ArgumentException.ThrowIfNullOrWhiteSpace(code, nameof(code));
             ArgumentException.ThrowIfNullOrWhiteSpace(name, nameof(name));
+            ArgumentOutOfRangeException.ThrowIfLessThan(examDurationInMinutes, 0, nameof(examDurationInMinutes));
 
             return new Certification
             {
                 Code = code,
-                Name = name
+                Name = name,
+                ExamDurationInMinutes = examDurationInMinutes
             };
         }
 
@@ -50,6 +53,18 @@
                 if (!_skillAreas.Any(sa => sa.Name.Equals(skillArea, StringComparison.OrdinalIgnoreCase)))
                     _skillAreas.Add(SkillArea.Create(skillArea));
             }
+        }
+
+        public int CalculateQuestionCount()
+        {
+            var questionCount = ExamDurationInMinutes / 1.5;
+            if (questionCount < 40)
+                return 40;
+
+            if (questionCount > 60)
+                return 60;
+
+            return (int)questionCount;
         }
 
         public void Deactivate()
